23ef5b3fa4e8d5e3ea9fe5a53ee4a930dae07c77
Naveenkumar247 <wwwvoiceofnaveen3@gmail.com>
2026-06-24 15:35:34 +0530

    Update posts.service.ts

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
-import { Post, PostDocument } from '../posts/schemas/post.schema';
+import { Post, PostDocument } from './schemas/post.schema'; // <-- Points precisely here now
+
 
 @Injectable()
 export class PostsService {
