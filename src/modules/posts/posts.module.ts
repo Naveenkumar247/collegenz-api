6df2a5240780032bf022c4727080945f6fea67e6
Naveenkumar247 <wwwvoiceofnaveen3@gmail.com>
2026-06-24 15:34:28 +0530

    Update posts.module.ts

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -2,21 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
-import { UsersModule } from '../users/users.module';
-import { Post, PostSchema } from '../users/schemas/post.schema';
-import { User, UserSchema } from '../users/schemas/user.schema';
-import { JwtModule } from '@nestjs/jwt';
+import { Post, PostSchema } from './schemas/post.schema'; // <-- Import schema
 
 @Module({
   imports: [
-    UsersModule, // Pulls model definitions safely
-    MongooseModule.forFeature([
-      { name: Post.name, schema: PostSchema },
-      { name: User.name, schema: UserSchema },
-    ]),
-    JwtModule, // Needed by Guards to verify signatures
+    // Registers the post data model structure within the MongoDB injection scope
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
   ],
   controllers: [PostsController],
   providers: [PostsService],
+  exports: [PostsService],
 })
 export class PostsModule {}
