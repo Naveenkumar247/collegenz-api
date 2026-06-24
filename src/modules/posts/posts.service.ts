5efc1f72e7b1ee7a8709441e60971e0a449c5769
Naveenkumar247 <wwwvoiceofnaveen3@gmail.com>
2026-06-24 15:29:53 +0530

    Fix import path for Post schema in posts.service.ts

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
-import { Post, PostDocument } from './schemas/post.schema';
+import { Post, PostDocument } from '../posts/schemas/post.schema';
 
 @Injectable()
 export class PostsService {
