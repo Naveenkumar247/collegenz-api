fb497d96ff8d58df7a0ce843c86099b83f8731d3
Naveenkumar247 <wwwvoiceofnaveen3@gmail.com>
2026-06-24 16:30:02 +0530

    Fix import path for PostSchema in posts.module.ts

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
-import { Post, PostSchema } from '@/modules/posts/schemas/post.schema';
+import { Post, PostSchema } from './schemas/post.schema';
 
 
 @Module({
