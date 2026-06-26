7c095a1a217388922996ac51705b9ace314a1704
Naveenkumar247 <wwwvoiceofnaveen3@gmail.com>
2026-06-26 17:53:58 +0530

    Update post.schema.ts

--- src/modules/posts/schema/post.schema.ts
+++ src/modules/posts/schema/post.schema.ts
@@ -3,7 +3,7 @@ import { Document, Schema as MongooseSchema } from 'mongoose';
 
 export type PostDocument = Post & Document;
 
-@Schema({ collection: 'users', strict: false }) // 🟢 strict: false prevents Mongoose from dropping fields
+@Schema({ collection: 'posts', strict: false, timestamps: true }) 
 export class Post {
   @Prop({ type: MongooseSchema.Types.Mixed })
   data: any;
@@ -18,17 +18,17 @@ export class Post {
   username: any;
 
   @Prop({ type: MongooseSchema.Types.Mixed })
-  likedBy: any;
-
-  @Prop({ type: MongooseSchema.Types.Mixed })
   postType: any;
 
-  @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }], default: [] })
-likes: Types.ObjectId[];
+  // 🟢 EXPLICIT METRIC TRACKING ARRAYS
+  @Prop({ type: [{ type: MongooseSchema.Types.ObjectId, ref: 'User' }], default: [] })
+  likes: MongooseSchema.Types.ObjectId[];
 
-@Prop({ type: [{ type: Types.ObjectId, ref: 'User' }], default: [] })
-savedBy: Types.ObjectId[];
-  
+  @Prop({ type: [{ type: MongooseSchema.Types.ObjectId, ref: 'User' }], default: [] })
+  savedBy: MongooseSchema.Types.ObjectId[];
+
+  @Prop({ type: [{ type: MongooseSchema.Types.ObjectId, ref: 'User' }], default: [] })
+  sharedBy: MongooseSchema.Types.ObjectId[];
 }
 
 export const PostSchema = SchemaFactory.createForClass(Post);
