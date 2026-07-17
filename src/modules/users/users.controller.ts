e482d8782dae5aacce0b34b9b347c7cbbc25c193
Naveenkumar247 <wwwvoiceofnaveen3@gmail.com>
2026-07-17 08:16:30 +0530

    Update users.controller.ts

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard'; // 🟢 FIXED: Points directly to the new file (no /guards folder)
 
-@Controller('api/v1/auth')
+@Controller('auth')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
