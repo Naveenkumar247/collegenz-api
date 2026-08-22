1c9e01b2ae1e54f724ac6b20c98e2235c4ef7a64
Naveenkumar247 <wwwvoiceofnaveen3@gmail.com>
2026-08-22 17:08:42 +0530

    Create featured-post.schema.ts

---
+++ src/modules/featured-posts/schema/featured-post.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type FeaturedPostDocument = FeaturedPost & Document;
+
+@Schema({ timestamps: true })
+export class FeaturedPost {
+  @Prop({ type: Types.ObjectId, ref: 'Post', required: true, unique: true })
+  postId: Types.ObjectId;
+
+  @Prop({ type: Number, default: 0 })
+  priority: number;
+
+  @Prop({ type: Date, default: null })
+  expiresAt: Date;
+}
+
+export const FeaturedPostSchema = SchemaFactory.createForClass(FeaturedPost);
