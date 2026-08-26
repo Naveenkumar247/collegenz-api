4d43603638c8b92fa2f5f00231867883ebe69dd1
Naveenkumar247 <wwwvoiceofnaveen3@gmail.com>
2026-08-26 07:36:15 +0530

    Add Post schema to FeaturedPostsModule

--- src/modules/featured-posts/featured-posts.module.ts
+++ src/modules/featured-posts/featured-posts.module.ts
@@ -3,14 +3,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { FeaturedPostsController } from './featured-posts.controller';
 import { FeaturedPostsService } from './featured-posts.service';
 import { FeaturedPost, FeaturedPostSchema } from './schema/featured-post.schema';
+import { Post, PostSchema } from '../posts/schema/post.schema'; // Adjust relative path to your Post schema
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: FeaturedPost.name, schema: FeaturedPostSchema },
+      { name: Post.name, schema: PostSchema }, // Required for .populate('postId')
     ]),
   ],
   controllers: [FeaturedPostsController],
   providers: [FeaturedPostsService],
+  exports: [FeaturedPostsService],
 })
 export class FeaturedPostsModule {}
