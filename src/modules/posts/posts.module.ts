2ce13d97a6dad0ed7ec70c9f69c0e39b0857f2b8
Naveenkumar247 <wwwvoiceofnaveen3@gmail.com>
2026-09-07 21:12:57 +0530

    Clean up PostsModule by removing unused Featured Schema
    Removed commented-out imports and registration for Featured Schema in PostsModule.

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -4,16 +4,12 @@ import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 import { Post, PostSchema } from './schema/post.schema';
 import { User, UserSchema } from '../users/schema/user.schema';
-// 🟢 FIXED: Import the Featured Schema here
-import { Featured, FeaturedSchema } from './schema/featured.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Post.name, schema: PostSchema },
       { name: User.name, schema: UserSchema },
-      // 🟢 FIXED: Register the Featured Schema so PostsService can use it
-      { name: Featured.name, schema: FeaturedSchema },
     ]),
   ],
   controllers: [PostsController],
