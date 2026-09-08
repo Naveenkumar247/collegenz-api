00cecba66ac5557e8a8038f4525aa228ed58b53a
Naveenkumar247 <wwwvoiceofnaveen3@gmail.com>
2026-09-08 14:27:51 +0530

    Refactor FeaturedPostsModule imports and schema usage

--- src/modules/featured-posts/featured-posts.module.ts
+++ src/modules/featured-posts/featured-posts.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FeaturedPostsController } from './featured-posts.controller';
 import { FeaturedPostsService } from './featured-posts.service';
-import { FeaturedPost, FeaturedPostSchema } from './schema/featured-post.schema';
-import { Post, PostSchema } from '../posts/schema/post.schema'; // Adjust relative path to your Post schema
+import {
+  FeaturedPost,
+  FeaturedPostSchema,
+} from './schema/featured-post.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: FeaturedPost.name, schema: FeaturedPostSchema },
-      { name: Post.name, schema: PostSchema }, // Required for .populate('postId')
+      {
+        name: FeaturedPost.name,
+        schema: FeaturedPostSchema,
+      },
     ]),
   ],
   controllers: [FeaturedPostsController],
